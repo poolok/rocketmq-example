45fa62402d075bb08776d882bcdb974e7706e87d
nongjb <nong-juan@163.com>
2015-12-16 17:57:11 +0800

    Initial commit

---
+++ src/main/java/com/alibaba/OnsProducerTest1.java
@@ -0,0 +1,49 @@
+package com.alibaba;
+
+import com.aliyun.openservices.ons.api.*;
+
+import java.util.Properties;
+
+/**
+ * 创建: rocketmq-example
+ * 描述:
+ * 编写者:农剑斌<nong-juan@163.com>
+ * 版本: 0.0.1
+ * 创建时间:15-12-16 下午5:40
+ */
+public class OnsProducerTest1 {
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put(PropertyKeyConst.ProducerId, "PID_NONGJIANBIN00001");
+        properties.put(PropertyKeyConst.AccessKey, "请输入AccessKey");
+        properties.put(PropertyKeyConst.SecretKey, "请输入SecretKey");
+        Producer producer = ONSFactory.createProducer(properties);
+
+        //在发送消息前，必须调用start方法来启动Producer，只需调用一次即可。
+        producer.start();
+        Message msg = new Message(
+                //Message Topic
+                "MY_ONS_TEST1",
+                //Message Tag,
+                //可理解为Gmail中的标签，对消息进行再归类，方便Consumer指定过滤条件在ONS服务器过滤
+                "TEST1",
+                //Message Body
+                //任何二进制形式的数据，ONS不做任何干预，需要Producer与Consumer协商好一致的序列化和反序列化方式
+                "Hello ONS".getBytes()
+        );
+
+        // 设置代表消息的业务关键属性，请尽可能全局唯一。
+        // 以方便您在无法正常收到消息情况下，可通过ONS Console查询消息并补发。
+        // 注意：不设置也不会影响消息正常收发
+        msg.setKey("TEST1");
+
+        //发送消息，只要不抛异常就是成功
+        SendResult sendResult = producer.send(msg);
+        System.out.println(sendResult);
+
+        // 在应用退出前，销毁Producer对象
+        // 注意：如果不销毁也没有问题
+        producer.shutdown();
+    }
+}
