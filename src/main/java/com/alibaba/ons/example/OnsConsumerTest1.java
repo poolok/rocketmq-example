ec22283efa6f763637d93ac1e8b663b80103fd03
nongjb <nong-juan@163.com>
2015-12-17 17:46:18 +0800

    Initial commit

---
+++ src/main/java/com/alibaba/ons/example/OnsConsumerTest1.java
@@ -0,0 +1,11 @@
+package com.alibaba.ons.example;
+
+/**
+ * 创建: rocketmq-example
+ * 描述:
+ * 编写者:农剑斌<nong-juan@163.com>
+ * 版本: 0.0.1
+ * 创建时间:15-12-17 下午5:45
+ */
+public class OnsConsumerTest1 {
+}
--- src/main/java/com/alibaba/ons/example/OnsProducerTest1.java
+++ src/main/java/com/alibaba/ons/example/OnsProducerTest1.java
@@ -1,4 +1,4 @@
-package com.alibaba;
+package com.alibaba.ons.example;
 
 import com.aliyun.openservices.ons.api.*;
 
@@ -15,19 +15,19 @@ public class OnsProducerTest1 {
 
     public static void main(String[] args) {
         Properties properties = new Properties();
-        properties.put(PropertyKeyConst.ProducerId, "PID_NONGJIANBIN00001");
-        properties.put(PropertyKeyConst.AccessKey, "请输入AccessKey");
-        properties.put(PropertyKeyConst.SecretKey, "请输入SecretKey");
+        properties.put(PropertyKeyConst.ProducerId, "PID_NJB_00001");
+        properties.put(PropertyKeyConst.AccessKey, "flQGIFXb7cmRTHbb");
+        properties.put(PropertyKeyConst.SecretKey, "L6tdBc48FKoSOJ4D2fngYqGvGPWqgx");
         Producer producer = ONSFactory.createProducer(properties);
 
         //在发送消息前，必须调用start方法来启动Producer，只需调用一次即可。
         producer.start();
         Message msg = new Message(
                 //Message Topic
-                "MY_ONS_TEST1",
+                "MyTest1",
                 //Message Tag,
                 //可理解为Gmail中的标签，对消息进行再归类，方便Consumer指定过滤条件在ONS服务器过滤
-                "TEST1",
+                "TagA",
                 //Message Body
                 //任何二进制形式的数据，ONS不做任何干预，需要Producer与Consumer协商好一致的序列化和反序列化方式
                 "Hello ONS".getBytes()
@@ -36,7 +36,7 @@ public class OnsProducerTest1 {
         // 设置代表消息的业务关键属性，请尽可能全局唯一。
         // 以方便您在无法正常收到消息情况下，可通过ONS Console查询消息并补发。
         // 注意：不设置也不会影响消息正常收发
-        msg.setKey("TEST1");
+        msg.setKey("ORDERID_100");
 
         //发送消息，只要不抛异常就是成功
         SendResult sendResult = producer.send(msg);
